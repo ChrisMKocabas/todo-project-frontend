76c4b6982a8ea836fdc673d5cec13fd5e11cf53b
Chris Kocabas <chriskocabas@outlook.com>
2023-08-02 22:01:55 -0400

    refactor

--- models/TodoType.ts
+++
@@ -1,8 +0,0 @@
-export type TodoType = {
-  description: string;
-  title: string;
-  status: boolean;
-  createdAt?: Date
-  _id: number;
-  user: number;
-};
--- pages/Home.tsx
+++
@@ -1,9 +0,0 @@
-const Home = () => {
-  return (
-    <div className="page-style align-self-center d-flex flex-column align-items-center justify-items-center">
-      Please create an account and login to start using the app.
-    </div>
-  );
-};
-
-export default Home;
